6a4378fa09c644a766c06698eb0222b06abedd9c
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-10-26 15:03:16 -0400

    1) Restore to initial commit 2) Upgrade to 2.2.0.RELEASE 3) Upgrade to Java 11 4) Removed reference to repo.spring.io milestone and snapshot repositories

---
+++ src/test/java/guru/springframework/blog/unmarshal/XmlToProductTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.blog.unmarshal;
+
+import guru.springframework.blog.domain.Product;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class XmlToProductTest {
+    private Product product;
+    @Test
+    public void testXmlToObject() throws JAXBException, FileNotFoundException {
+        File file = new File("product.xml");
+        JAXBContext jaxbContext = JAXBContext.newInstance(Product.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        product = (Product) unmarshaller.unmarshal(file);
+        System.out.println(product);
+    }
+}
